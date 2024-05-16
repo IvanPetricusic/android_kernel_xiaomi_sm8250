1ca2f1bd2f59317f490aed18325c8020c4b12a87
EmanuelCN <emanuelghub@gmail.com>
2024-05-16 18:22:56 +0300

    Revert "sched/eevdf: Use sched_attr::sched_runtime to set"
    This reverts commit d5261db87379051de6b6e7d5712e7a9ee98f9c44.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -990,8 +990,7 @@ static void update_deadline(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	 * nice) while the request time r_i is determined by
 	 * sysctl_sched_base_slice.
 	 */
-	if (!se->custom_slice)
-		se->slice = sysctl_sched_base_slice;
+	se->slice = sysctl_sched_base_slice;
 
 	/*
 	 * EEVDF: vd_i = ve_i + r_i / w_i
@@ -4677,8 +4676,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	u64 vslice, vruntime = avg_vruntime(cfs_rq);
 	s64 lag = 0;
 
-	if (!se->custom_slice)
-		se->slice = sysctl_sched_base_slice;
+	se->slice = sysctl_sched_base_slice;
 	vslice = calc_delta_fair(se->slice, se);
 
 	/*
